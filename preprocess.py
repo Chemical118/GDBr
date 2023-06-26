bb4154e8addb04a5ccefd03ef56aaaff001f9703
wowo <wowo0118@sasa.hs.kr>
2023-06-26 16:08:56 +0900

    Add function at preprocess

--- preprocess.py
+++ preprocess.py
@@ -3,29 +3,14 @@ from pyfaidx import Fasta
 from my_tqdm import p_map
 
 import subprocess
+import shutil
 import vcfpy
 import csv
 import os
 
+blastn_fmt = ['pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'bitscore']
 
-def get_real_sv(record, sv_find_len=2000):
-    tid = record.ID[0].split('.')[1:-1][0]
-    
-    chrom = record.CHROM
-    if tid not in {'DEL', 'INS'}:
-        return 'UNSUP_ID',
-
-    ref_seq = Fasta(ref_loc, build_index=False)
-    qry_seq = Fasta(qry_loc, build_index=False)
-
-    ref_start = record.POS + 1
-    ref_end = record.INFO['END']
-    ref_len = abs(record.INFO['SVLEN'])
-    sv_id = record.ID[0]
-    
-    start_temp_seq = ref_seq[record.CHROM][ref_start - sv_find_len - 1:ref_start - 1]
-    end_temp_seq = ref_seq[record.CHROM][ref_end:ref_end + sv_find_len]
-
+def get_blast_result(start_temp_seq, end_temp_seq, sv_find_len, sv_id, chrom):
     start_temp_seq_loc = os.path.join(qryworkdir, f'ref_start_{sv_id}.fasta')
     end_temp_seq_loc = os.path.join(qryworkdir, f'ref_end_{sv_id}.fasta')
 
@@ -37,9 +22,8 @@ def get_real_sv(record, sv_find_len=2000):
         f.write('>' + end_temp_seq.fancy_name + '\n')
         f.write(str(end_temp_seq))
 
-    blastn_fmt = ['pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'bitscore']
-    start_result = subprocess.run(['blastn', '-db', os.path.join(dbdir, record.CHROM), '-query', start_temp_seq_loc, '-strand', 'plus', '-outfmt', '10 ' + ' '.join(blastn_fmt)], capture_output=True, text=True)
-    end_result = subprocess.run(['blastn', '-db', os.path.join(dbdir, record.CHROM), '-query', end_temp_seq_loc, '-strand', 'plus', '-outfmt', '10 ' + ' '.join(blastn_fmt)], capture_output=True, text=True)
+    start_result = subprocess.run(['blastn', '-db', os.path.join(dbdir, chrom), '-query', start_temp_seq_loc, '-strand', 'plus', '-outfmt', '10 ' + ' '.join(blastn_fmt)], capture_output=True, text=True)
+    end_result = subprocess.run(['blastn', '-db', os.path.join(dbdir, chrom), '-query', end_temp_seq_loc, '-strand', 'plus', '-outfmt', '10 ' + ' '.join(blastn_fmt)], capture_output=True, text=True)
 
     start_result_list =  [] if start_result.stdout == '' else map(lambda t: list(map(eval, t.split(','))), start_result.stdout[:-1].split('\n'))
     end_result_list = [] if end_result.stdout == '' else map(lambda t: list(map(eval, t.split(','))), end_result.stdout[:-1].split('\n'))
@@ -49,10 +33,31 @@ def get_real_sv(record, sv_find_len=2000):
 
     start_filter_result = list(filter(lambda t: t[1] > sv_find_len * 0.90, start_result_list))
     end_filter_result = list(filter(lambda t: t[1] > sv_find_len * 0.90, end_result_list))
-    
-    if len(start_filter_result) != 1 or len(end_filter_result) != 1:
-        return f'FND_IDX:({len(start_filter_result)}, {len(end_filter_result)})',
 
+    return start_filter_result, end_filter_result
+
+def get_blast_single_result(ref_temp_seq, qry_temp_seq, sv_find_len, sv_id, filter_func):
+    ref_temp_seq_loc = os.path.join(qryworkdir, f'sub_ref_{sv_id}.fasta')
+    qry_temp_seq_loc = os.path.join(qryworkdir, f'sub_qry_{sv_id}.fasta')
+
+    with open(ref_temp_seq_loc, 'w') as f:
+        f.write('>' + ref_temp_seq.fancy_name + '\n')
+        f.write(str(ref_temp_seq))
+
+    with open(qry_temp_seq_loc, 'w') as f:
+        f.write('>' + qry_temp_seq.fancy_name + '\n')
+        f.write(str(qry_temp_seq))
+
+    sub_result = subprocess.run(['blastn', '-subject', qry_temp_seq_loc, '-query', ref_temp_seq_loc, '-strand', 'plus', '-outfmt', '10 ' + ' '.join(blastn_fmt)], capture_output=True, text=True)
+    sub_result_list =  [] if sub_result.stdout == '' else map(lambda t: list(map(eval, t.split(','))), sub_result.stdout[:-1].split('\n'))
+    sub_filter_result = sorted(filter(lambda t: t[1] > sv_find_len * 0.90 and filter_func(t), sub_result_list), key=lambda t: t[8], reverse=True)
+
+    os.remove(ref_temp_seq_loc)
+    os.remove(qry_temp_seq_loc)
+
+    return sub_filter_result
+
+def get_corrected_location(ref_start, ref_end, sv_find_len, start_filter_result, end_filter_result):
     ref_start -= sv_find_len - start_filter_result[0][5]
     ref_end += end_filter_result[0][4] - 1
     ref_len = ref_end - ref_start + 1
@@ -60,45 +65,59 @@ def get_real_sv(record, sv_find_len=2000):
     qry_start = start_filter_result[0][7] + 1
     qry_end = end_filter_result[0][6] - 1
     qry_len = qry_end - qry_start + 1
+    return ref_start, ref_end, ref_len, qry_start, qry_end, qry_len
+
+def get_correctrd_location(ref_temp_seq, qry_temp_seq, ref_start, ref_end, qry_start, qry_end):
+    base_ref_start = ref_start
+    base_qry_start = qry_start
+
+    while ref_start < ref_end + 1 and qry_start < qry_end and ref_temp_seq[ref_start - base_ref_start] == qry_temp_seq[qry_start - base_qry_start]:
+        ref_start += 1
+        qry_start += 1
+
+    while ref_start - 1 < ref_end and qry_start - 1 < qry_end and ref_temp_seq[ref_end - base_ref_start] == qry_temp_seq[qry_end - base_qry_start]:
+        ref_end -= 1
+        qry_end -= 1
+
+    ref_len = ref_end - ref_start + 1
+    qry_len = qry_end - qry_start + 1
+    return ref_start, ref_end, ref_len, qry_start, qry_end, qry_len
+
+def get_real_sv(record, sv_find_len=2000):
+    tid = record.ID[0].split('.')[1:-1][0]
+    
+    if tid not in {'DEL', 'INS'}:
+        return 'UNSUP_ID',
+
+    ref_seq = Fasta(ref_loc, build_index=False)
+    qry_seq = Fasta(qry_loc, build_index=False)
+
+    ref_start, ref_end, ref_len, sv_id, chrom = record.POS + 1, record.INFO['END'], abs(record.INFO['SVLEN']), record.ID[0], record.CHROM
+    
+    start_temp_seq = ref_seq[chrom][ref_start - sv_find_len - 1:ref_start - 1]
+    end_temp_seq = ref_seq[chrom][ref_end:ref_end + sv_find_len]
+
+    start_filter_result, end_filter_result = get_blast_result(start_temp_seq, end_temp_seq, sv_find_len, sv_id, chrom)
+    
+    if len(start_filter_result) != 1 or len(end_filter_result) != 1:
+        return f'FND_IDX:({len(start_filter_result)}, {len(end_filter_result)})',
+
+    ref_start, ref_end, ref_len, qry_start, qry_end, qry_len = get_corrected_location(ref_start, ref_end, sv_find_len, start_filter_result, end_filter_result)
 
     call_err_try = 3
     while call_err_try > 0 and qry_len < 0 and start_filter_result[0][6] < end_filter_result[0][6] and start_filter_result[0][7] < end_filter_result[0][7]:
         ref_end -= qry_len
         qry_end -= qry_len
         
-        start_temp_seq = ref_seq[record.CHROM][ref_start - sv_find_len - 1:ref_start - 1]
-        end_temp_seq = ref_seq[record.CHROM][ref_end:ref_end + sv_find_len]
+        start_temp_seq = ref_seq[chrom][ref_start - sv_find_len - 1:ref_start - 1]
+        end_temp_seq = ref_seq[chrom][ref_end:ref_end + sv_find_len]
 
-        with open(start_temp_seq_loc, 'w') as f:
-            f.write('>' + start_temp_seq.fancy_name + '\n')
-            f.write(str(start_temp_seq))
-
-        with open(end_temp_seq_loc, 'w') as f:
-            f.write('>' + end_temp_seq.fancy_name + '\n')
-            f.write(str(end_temp_seq))
-
-        start_result = subprocess.run(['blastn', '-db', os.path.join(dbdir, record.CHROM), '-query', start_temp_seq_loc, '-strand', 'plus', '-outfmt', '10 ' + ' '.join(blastn_fmt)], capture_output=True, text=True)
-        end_result = subprocess.run(['blastn', '-db', os.path.join(dbdir, record.CHROM), '-query', end_temp_seq_loc, '-strand', 'plus', '-outfmt', '10 ' + ' '.join(blastn_fmt)], capture_output=True, text=True)
-
-        start_result_list =  [] if start_result.stdout == '' else map(lambda t: list(map(eval, t.split(','))), start_result.stdout[:-1].split('\n'))
-        end_result_list = [] if end_result.stdout == '' else map(lambda t: list(map(eval, t.split(','))), end_result.stdout[:-1].split('\n'))
-
-        os.remove(start_temp_seq_loc)
-        os.remove(end_temp_seq_loc)
-
-        start_filter_result = list(filter(lambda t: t[1] > sv_find_len * 0.90, start_result_list))
-        end_filter_result = list(filter(lambda t: t[1] > sv_find_len * 0.90, end_result_list))
+        start_filter_result, end_filter_result = get_blast_result(start_temp_seq, end_temp_seq, sv_find_len, sv_id, chrom)
         
         if len(start_filter_result) != 1 or len(end_filter_result) != 1:
             return f'FND_IDX:({len(start_filter_result)}, {len(end_filter_result)})',
 
-        ref_start -= sv_find_len - start_filter_result[0][5]
-        ref_end += end_filter_result[0][4] - 1
-        ref_len = ref_end - ref_start + 1
-
-        qry_start = start_filter_result[0][7] + 1
-        qry_end = end_filter_result[0][6] - 1
-        qry_len = qry_end - qry_start + 1
+        ref_start, ref_end, ref_len, qry_start, qry_end, qry_len = get_corrected_location(ref_start, ref_end, sv_find_len, start_filter_result, end_filter_result)
 
         call_err_try -= 1
     
@@ -106,26 +125,13 @@ def get_real_sv(record, sv_find_len=2000):
     if ref_len > 0 and qry_len > 0:
         check_len = min(ref_len, qry_len)
 
-        start_ref_temp_seq = ref_seq[record.CHROM][ref_start - sv_find_len - 1:ref_start - 1 + check_len + (min(abs(ref_len - qry_len), 10) if ref_len > check_len else 0)]
-        start_qry_temp_seq = qry_seq[record.CHROM][qry_start - sv_find_len - 1:qry_start - 1 + check_len + (min(abs(ref_len - qry_len), 10) if qry_len > check_len else 0)]
-        
-        start_ref_temp_seq_loc = os.path.join(qryworkdir, f'sub_ref_start_{sv_id}.fasta')
-        start_qry_temp_seq_loc = os.path.join(qryworkdir, f'sub_qry_start_{sv_id}.fasta')
+        start_ref_check_len = check_len + (min(abs(ref_len - qry_len), 10) if ref_len > check_len else 0)
+        start_qry_check_len = check_len + (min(abs(ref_len - qry_len), 10) if qry_len > check_len else 0)
 
-        with open(start_ref_temp_seq_loc, 'w') as f:
-            f.write('>' + start_ref_temp_seq.fancy_name + '\n')
-            f.write(str(start_ref_temp_seq))
+        start_ref_temp_seq = ref_seq[chrom][ref_start - sv_find_len - 1:ref_start - 1 + start_ref_check_len]
+        start_qry_temp_seq = qry_seq[chrom][qry_start - sv_find_len - 1:qry_start - 1 + start_qry_check_len]
 
-        with open(start_qry_temp_seq_loc, 'w') as f:
-            f.write('>' + start_qry_temp_seq.fancy_name + '\n')
-            f.write(str(start_qry_temp_seq))
-
-        start_sub_result = subprocess.run(['blastn', '-subject', start_qry_temp_seq_loc, '-query', start_ref_temp_seq_loc, '-strand', 'plus', '-outfmt', '10 ' + ' '.join(blastn_fmt)], capture_output=True, text=True)
-        start_sub_result_list =  [] if start_sub_result.stdout == '' else map(lambda t: list(map(eval, t.split(','))), start_sub_result.stdout[:-1].split('\n'))
-        start_sub_filter_result = sorted(filter(lambda t: t[1] > sv_find_len * 0.90 and t[5] >= sv_find_len and t[7] >= sv_find_len, start_sub_result_list), key=lambda t: t[8], reverse=True)
-
-        os.remove(start_ref_temp_seq_loc)
-        os.remove(start_qry_temp_seq_loc)
+        start_sub_filter_result = get_blast_single_result(start_ref_temp_seq, start_qry_temp_seq, sv_find_len, sv_id, lambda t: t[5] >= sv_find_len and t[7] >= sv_find_len)
 
         if len(start_sub_filter_result) > 0:
             ref_start += start_sub_filter_result[0][5] - sv_find_len
@@ -141,26 +147,10 @@ def get_real_sv(record, sv_find_len=2000):
         end_ref_check_len = check_len + (min(abs(ref_len - qry_len), 10) if ref_len > check_len else 0)
         end_qry_check_len = check_len + (min(abs(ref_len - qry_len), 10) if qry_len > check_len else 0)
         
-        end_ref_temp_seq = ref_seq[record.CHROM][ref_end - end_ref_check_len:ref_end + sv_find_len]
-        end_qry_temp_seq = qry_seq[record.CHROM][qry_end - end_qry_check_len:qry_end + sv_find_len]
+        end_ref_temp_seq = ref_seq[chrom][ref_end - end_ref_check_len:ref_end + sv_find_len]
+        end_qry_temp_seq = qry_seq[chrom][qry_end - end_qry_check_len:qry_end + sv_find_len]
 
-        end_ref_temp_seq_loc = os.path.join(qryworkdir, f'sub_ref_end_{sv_id}.fasta')
-        end_qry_temp_seq_loc = os.path.join(qryworkdir, f'sub_qry_end_{sv_id}.fasta')
-
-        with open(end_ref_temp_seq_loc, 'w') as f:
-            f.write('>' + end_ref_temp_seq.fancy_name + '\n')
-            f.write(str(end_ref_temp_seq))
-
-        with open(end_qry_temp_seq_loc, 'w') as f:
-            f.write('>' + end_qry_temp_seq.fancy_name + '\n')
-            f.write(str(end_qry_temp_seq))
-
-        end_sub_result = subprocess.run(['blastn', '-subject', end_qry_temp_seq_loc, '-query', end_ref_temp_seq_loc, '-strand', 'plus', '-outfmt', '10 ' + ' '.join(blastn_fmt)], capture_output=True, text=True)
-        end_sub_result_list = [] if end_sub_result.stdout == '' else map(lambda t: list(map(eval, t.split(','))), end_sub_result.stdout[:-1].split('\n'))
-        end_sub_filter_result = sorted(filter(lambda t: t[1] > sv_find_len * 0.90 and t[4] <= end_ref_check_len + 1 and t[6] <= end_qry_check_len + 1, end_sub_result_list), key=lambda t: t[8], reverse=True)
-        
-        os.remove(end_ref_temp_seq_loc)
-        os.remove(end_qry_temp_seq_loc)
+        end_sub_filter_result = get_blast_single_result(end_ref_temp_seq, end_qry_temp_seq, sv_find_len, sv_id, lambda t: t[4] <= end_ref_check_len + 1 and t[6] <= end_qry_check_len + 1)
 
         if len(end_sub_filter_result) > 0:
             ref_end -= end_ref_check_len + 1 - end_sub_filter_result[0][4]
@@ -171,23 +161,11 @@ def get_real_sv(record, sv_find_len=2000):
 
     # substitution index test
     if ref_len > 0 and qry_len > 0:
-        base_ref_start = ref_start
-        base_qry_start = qry_start
+        ref_temp_seq = str(ref_seq[chrom][ref_start - 1:ref_end]).upper()
+        qry_temp_seq = str(qry_seq[chrom][qry_start - 1:qry_end]).upper()
 
-        ref_temp_seq = str(ref_seq[record.CHROM][ref_start - 1:ref_end]).upper()
-        qry_temp_seq = str(qry_seq[record.CHROM][qry_start - 1:qry_end]).upper()
-
-        while ref_start < ref_end + 1 and qry_start < qry_end and ref_temp_seq[ref_start - base_ref_start] == qry_temp_seq[qry_start - base_qry_start]:
-            ref_start += 1
-            qry_start += 1
-
-        while ref_start - 1 < ref_end and qry_start - 1 < qry_end and ref_temp_seq[ref_end - base_ref_start] == qry_temp_seq[qry_end - base_qry_start]:
-            ref_end -= 1
-            qry_end -= 1
-
-        ref_len = ref_end - ref_start + 1
-        qry_len = qry_end - qry_start + 1
-    
+        ref_start, ref_end, ref_len, qry_start, qry_end, qry_len = get_correctrd_location(ref_temp_seq, qry_temp_seq, ref_start, ref_end, qry_start, qry_end)
+        
     if qry_len < 0:
         sv_name = 'ERR_POS'
 
@@ -203,11 +181,13 @@ def get_real_sv(record, sv_find_len=2000):
     else:
         sv_name = 'DEL'
 
-    return sv_name, record.CHROM, ref_start, ref_end, qry_start, qry_end    
+    return sv_name, chrom, ref_start, ref_end, qry_start, qry_end    
 
 # working directory setting
 workdir = 'data'
+force = True
 
+shutil.rmtree(workdir)
 if not os.path.isdir(workdir):
     try:
         os.mkdir(workdir)
@@ -250,9 +230,9 @@ for qry_ind, (qry_loc, vcf_loc) in enumerate(zip(qry_loc_list, vcf_loc_list)):
     vcf_tot_data = [record for record in vcfpy.Reader.from_path(vcf_loc) if record.CHROM in ref_chr_list]
 
     sv_find_len = 2000
-    sv_list = p_map(partial(get_real_sv, sv_find_len=sv_find_len), vcf_tot_data, num_cpus=14)
+    sv_list = p_map(partial(get_real_sv, sv_find_len=sv_find_len), vcf_tot_data, num_cpus=15)
     
-    with open(f'sv_call_{qry_ind}.csv', 'w') as f:
+    with open(f'sv_call_{qry_ind}_test.csv', 'w') as f:
         cf = csv.writer(f)
         cf.writerow(('ID', 'SV_TYPE', 'CHROM', 'REF_START', 'REF_END', 'QRY_START', 'QRY_END'))
         cf.writerows([[i] + list(v) for i, v in enumerate(sv_list)])
